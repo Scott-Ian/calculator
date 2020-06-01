d296beba8ed893713ff65fdf7caa552d6c3aac37
Ian Gregg <iangregg188@gmail.com>
2020-06-01 11:25:35 -0700

    Added a function that converted celsius to Fahrenheit <Dgjackson01@gmail.com> Co-authored-by: Ian Gregg <iangregg188@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,11 @@ const result = Add(number1, number2);
 const result2 = Divide(number1, number2)
 alert(result);
 alert(result2)
+
+const TempC = function(number1){
+  return number1 * (9/5) + 32
+}
+
+const number3 = parseInt(prompt("Enter Celsius temp: "))
+const result3 = TempC(number1)
+alert(result3 + "Fahrenheit")
