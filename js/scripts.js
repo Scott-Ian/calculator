4db08b6ed2154434179097f193610a684b80efe9
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-03 13:13:25 -0700

    add html and JS logic for multiply, divide and subtract
    Co-authored-by: Ian Scott <chesnekov@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -17,9 +17,39 @@ const divide = function(number1, number2) {
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
-    const number1 = parseInt($("#num1").val());
-    const number2 = parseInt($("#num2").val());
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
     const addResult = add(number1, number2);
     $("#addOutput").text(addResult);
+  });
+});
+
+$(document).ready(function() {
+  $("form#sub").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#sub1").val());
+    const number2 = parseInt($("#sub2").val());
+    const subResult = subtract(number1, number2);
+    $("#subOutput").text(subResult);
+  });
+});
+
+$(document).ready(function() {
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#multiply1").val());
+    const number2 = parseInt($("#multiply2").val());
+    const multiplyResult = multiply(number1, number2);
+    $("#multiplyOutput").text(multiplyResult);
+  });
+});
+
+$(document).ready(function() {
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#divide1").val());
+    const number2 = parseInt($("#divide2").val());
+    const divideResult = divide(number1, number2);
+    $("#divideOutput").text(divideResult);
   });
 });
